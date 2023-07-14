34f80c3187263e205d4296f8185b610e866c9096
Serhii Myshko <sergeiown@gmail.com>
2023-07-15 00:52:39 +0300

    add additional comments

--- script_v2.bat
+++ script_v2.bat
@@ -1,5 +1,8 @@
 @echo off
 color 08
+
+REM Enable delayed variable expansion
+REM This allows obtaining updated variable values inside loops and code blocks
 setlocal enabledelayedexpansion
 
 REM Check if Ghostscript is installed
@@ -7,11 +10,14 @@ where gswin64c.exe >nul 2>&1
 if errorlevel 1 (
     cls
     color 0C
+
     echo Ghostscript is not installed.
     echo Please download and install Ghostscript from the following link:
     echo https://ghostscript.com/releases/gsdnld.html
+
     pause
     color
+
     exit /b
 )
 
@@ -20,6 +26,9 @@ echo Current Ghostscript version:
 gswin64c.exe --version
 echo All right, let's begin.
 echo.
+
+REM Pause for 3 seconds and prompt for path input
+timeout /t 3 >nul
 
 echo Enter the path to the directory with PDF files:
 echo.

2161ba3b558a4a862dca0383f0990b1543c9ac4f
sergeiown <sergeiown@gmail.com>
2024-10-23 17:55:37 +0300

    Ensure the reliability and flexibility of determining the system language using several sources of information in case one of them is unavailable

--- modules/language.bat
+++ modules/language.bat
@@ -2,21 +2,57 @@
 
 @echo off
 
+set "LOCALE="
 for /f %%A in ('wmic os get locale ^| find "0"') do set "LOCALE=%%A"
+
+if "%LOCALE%"=="" (
+    for /f "tokens=3" %%A in ('reg query "HKCU\Control Panel\International" /v LocaleName 2^>nul') do set "LOCALE=%%A"
+)
+
+if "%LOCALE%"=="" (
+    for /f %%A in ('powershell -Command "(Get-Culture).Name"') do set "LOCALE=%%A"
+)
+
+if "%LOCALE%"=="" (
+    for /f "tokens=2 delims=:" %%A in ('systeminfo ^| find "System Locale"') do (
+        for /f "tokens=1 delims=;" %%B in ("%%A") do (
+            for /f "tokens=* delims= " %%C in ("%%B") do set "LOCALE=%%C"
+        )
+    )
+)
+
+chcp 65001 >nul
 if "%LOCALE%"=="0422" (
-    chcp 65001 >nul
     echo Мова інтерфейсу: українська >> %outputFile%
     set "file_name=messages/messages_ukrainian.txt"
     echo.
     cls
 ) else if "%LOCALE%"=="0419" (
-    chcp 65001 >nul
+    echo Язык интерфейса: русский >> %outputFile%
+    set "file_name=messages/messages_russian.txt"
+    echo.
+    cls
+) else if "%LOCALE%"=="uk-UA" (
+    echo Мова інтерфейсу: українська >> %outputFile%
+    set "file_name=messages/messages_ukrainian.txt"
+    echo.
+    cls
+) else if "%LOCALE%"=="ru-RU" (
+    echo Язык интерфейса: русский >> %outputFile%
+    set "file_name=messages/messages_russian.txt"
+    echo.
+    cls
+) else if "%LOCALE%"=="uk" (
+    echo Мова інтерфейсу: українська >> %outputFile%
+    set "file_name=messages/messages_ukrainian.txt"
+    echo.
+    cls
+) else if "%LOCALE%"=="ru" (
     echo Язык интерфейса: русский >> %outputFile%
     set "file_name=messages/messages_russian.txt"
     echo.
     cls
 ) else (
-    chcp 65001 >nul
     echo Interface language: English >> %outputFile%
     set "file_name=messages/messages_english.txt"
     echo.
